ef224d0d837d8e893c59810a2c370362c0b48865
nevaero <nevaero@protonmail.com>
2024-05-20 10:39:12 +0200

    first draft

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'nutmeg': '#854632',
+                'dark-raspberry': '#7b284f',
+                'rose-white': '#fff5fa',
+                'eggshell': '#f3e6d8',
+                'light-grey': '#e4ded8',
+                'wenge-brown': '#5f574e',
+                'dark-charcoal': '#302d2c',
+            },
+        },
+    },
+    plugins: [],
+}
+
